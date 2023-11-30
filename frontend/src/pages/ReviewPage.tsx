85ced9f785cbf6cf0b4d11293e625a99ef7a97a3
PabloChristianno <pablochristianno@gmail.com>
2023-11-30 19:48:08 -0300

    correções nas estrelas

--- frontend/src/pages/ReviewPage.tsx
+++ frontend/src/pages/ReviewPage.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState,  } from 'react';
+import Rating from '@mui/material/Rating';
 import { useParams } from 'react-router-dom';
 import allProducts from '../assets/all_products';
 import './ReviewPage.css';
@@ -7,6 +9,10 @@ import { Link } from 'react-router-dom';
 const ReviewPage: React.FC = () => {
   const { productId } = useParams<{ productId: string }>();
   const product = allProducts.find(item => item.id.toString() === productId);
+  const [value, setValue] = useState("");
+  const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
+  const [value3, setValue3] = useState("");
 
   if (!product) {
     return <div>Produto não encontrado.</div>;
@@ -22,56 +28,44 @@ const ReviewPage: React.FC = () => {
             <span>{product.name}</span>
           </div>
           <h3>o que você achou do produto?</h3>
-          <div class="rate">
-            <input type="radio" id="star5" name="rate" value="5" />
-            <label for="star5" title="text">5 stars</label>
-            <input type="radio" id="star4" name="rate" value="4" />
-            <label for="star4" title="text">4 stars</label>
-            <input type="radio" id="star3" name="rate" value="3" />
-            <label for="star3" title="text">3 stars</label>
-            <input type="radio" id="star2" name="rate" value="2" />
-            <label for="star2" title="text">2 stars</label>
-            <input type="radio" id="star1" name="rate" value="1" />
-            <label for="star1" title="text">1 star</label>
+          <div className='ratting'>
+            <Rating
+              name="simple-controlled"
+              value ={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+            />
           </div>
           <h3>custo-benefício</h3>
-          <div class="rate">
-            <input type="radio" id="star5" name="rate" value="5" />
-            <label for="star5" title="text">5 stars</label>
-            <input type="radio" id="star4" name="rate" value="4" />
-            <label for="star4" title="text">4 stars</label>
-            <input type="radio" id="star3" name="rate" value="3" />
-            <label for="star3" title="text">3 stars</label>
-            <input type="radio" id="star2" name="rate" value="2" />
-            <label for="star2" title="text">2 stars</label>
-            <input type="radio" id="star1" name="rate" value="1" />
-            <label for="star1" title="text">1 star</label>
+          <div className='ratting'>
+            <Rating
+              name="simple-controlled"
+              value ={value1}
+              onChange={(event, newValue) => {
+                setValue1(newValue);
+              }}
+            />
           </div>
           <h3>qualidade</h3>
-          <div class="rate">
-            <input type="radio" id="star5" name="rate" value="5" />
-            <label for="star5" title="text">5 stars</label>
-            <input type="radio" id="star4" name="rate" value="4" />
-            <label for="star4" title="text">4 stars</label>
-            <input type="radio" id="star3" name="rate" value="3" />
-            <label for="star3" title="text">3 stars</label>
-            <input type="radio" id="star2" name="rate" value="2" />
-            <label for="star2" title="text">2 stars</label>
-            <input type="radio" id="star1" name="rate" value="1" />
-            <label for="star1" title="text">1 star</label>
+          <div className='ratting'>
+            <Rating
+              name="simple-controlled"
+              value ={value2}
+              onChange={(event, newValue) => {
+                setValue2(newValue);
+              }}
+            />
           </div>
           <h3>tamanho</h3>
-          <div class="rate">
-            <input type="radio" id="star5" name="rate" value="5" />
-            <label for="star5" title="text">5 stars</label>
-            <input type="radio" id="star4" name="rate" value="4" />
-            <label for="star4" title="text">4 stars</label>
-            <input type="radio" id="star3" name="rate" value="3" />
-            <label for="star3" title="text">3 stars</label>
-            <input type="radio" id="star2" name="rate" value="2" />
-            <label for="star2" title="text">2 stars</label>
-            <input type="radio" id="star1" name="rate" value="1" />
-            <label for="star1" title="text">1 star</label>
+          <div className='ratting'>
+            <Rating
+              name="simple-controlled"
+              value ={value3}
+              onChange={(event, newValue) => {
+                setValue3(newValue);
+              }}
+            />
           </div>
         </div>
         <div className="review-form">
