54f8bf21e39aa7d4ed78227895086b205d427397
fellipepcs <fellipy.02@hotmail.com>
2023-11-30 09:16:58 -0300

    feat: adicionado models completa de produto

--- backend/americanas/models/Produto.py
+++ backend/americanas/models/Produto.py
@@ -1,6 +1,16 @@
 from django.db import models
 from abstract_factory import Base
+from americanas.models import Usuario
+
 
 class Produto(Base):
     data_pedido = models.DateTimeField(auto_now_add=True)
     status_pedido = models.CharField(max_length=100)
+    detalhes_pedido = models.CharField(max_length=100)
+    preco_total = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade = models.IntegerField()
+    estoque = models.IntegerField()
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'produto'
