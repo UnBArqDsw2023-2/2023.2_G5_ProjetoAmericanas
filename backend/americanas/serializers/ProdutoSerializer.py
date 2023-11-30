a07488a14aa84ca3bd015c5eac7db910cb33f2c6
fellipepcs <fellipy.02@hotmail.com>
2023-11-30 09:21:56 -0300

    feat: validado dados de produto em serializer

--- backend/americanas/serializers/ProdutoSerializer.py
+++ backend/americanas/serializers/ProdutoSerializer.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from americanas.models import Produto
 
 class ProdutoSerializer(serializers.ModelSerializer):
+    categoria = serializers.CharField()
+    nome = serializers.CharField()
+    tipo = serializers.CharField()
+    preco = serializers.DecimalField(max_digits=10, decimal_places=2)
+    desconto = serializers.FloatField()
     class Meta:
         model = Produto
         fields = '__all__'
