db14d6c6ff4af4db917874da5c123aa37bd4e244
Leonardo M. Miranda <leonardomichalskim@gmail.com>
2023-12-02 21:56:09 -0300

    abafar warning 'MODULE_LEVEL_DIRECTIVE' ao 'buildar' frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return
+        }
+        warn(warning)
+      }
+    }
+  },
   plugins: [react()],
   server: {
     port: 3000,
